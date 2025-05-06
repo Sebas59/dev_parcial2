147d6148ed3d3b65b45001beabeae17ea9b67e22
Sebastian lopez <juancholour@gmail.com>
2025-05-05 20:45:20 -0500

    se corrigio error en la conexion con clever

--- utils/connection_db.py
+++ utils/connection_db.py
@@ -1,17 +1,16 @@
-'''Este es el archivo con la conexión a la DB.'''
 import os
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
-from data.models import Usuario
+from data.models import Usuario  
 
 
-CLEVER_DB= "postgresql+asyncpg://postgresql://u2bsfq4covw34iuqcyzx:cS0GdiA73mAZkRrjd4bAuuLRvoFe19@bxs86dwc5oftbf4m2glf-postgresql.services.clever-cloud.com:50013/bxs86dwc5oftbf4m2glf"
+CLEVER_DB = "postgresql+asyncpg://u2bsfq4covw34iuqcyzx:cS0GdiA73mAZkRrjd4bAuuLRvoFe19@bxs86dwc5oftbf4m2glf-postgresql.services.clever-cloud.com:50013/bxs86dwc5oftbf4m2glf"
 
-engine : AsyncEngine = create_async_engine(CLEVER_DB, echo=True)
-async_session =sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+engine: AsyncEngine = create_async_engine(CLEVER_DB, echo=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 async def init_db():
     async with engine.begin() as conn:
