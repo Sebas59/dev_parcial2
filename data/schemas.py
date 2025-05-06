0e6ec18290b76f32b3d780ba7db8b9ea594ea5bf
Sebastian lopez <juancholour@gmail.com>
2025-05-06 18:33:15 -0500

    se agregaron los modelos de tareas

--- data/schemas.py
+++ data/schemas.py
@@ -1,8 +1,24 @@
 from sqlmodel import SQLModel
-from data.models import EstadoUsuario
+from data.models import *
+from datetime import datetime
 
 class UsuarioCreate(SQLModel):
     nombre: str
     email: str
     estado: EstadoUsuario
     premium: bool = False
+
+class TareaCreate(SQLModel):
+    nombre: str
+    descripcion: str
+    estado: EstadoTarea
+    Usuario_id: int
+
+class TareaRead(SQLModel):
+    id: int
+    nombre: str
+    descripcion: str
+    fecha_creacion: datetime
+    fecha_modificacion: datetime
+    estado: EstadoTarea
+    Usuario_id: int
