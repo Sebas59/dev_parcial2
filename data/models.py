988a2d348e476e302f09d541e425f7491dbc89a2
Sebastian lopez <juancholour@gmail.com>
2025-05-05 20:01:03 -0500

    se agregaron los modelos

--- data/models.py
+++ data/models.py
@@ -1,4 +1,15 @@
-'''Aqui debes consignar el modelo que se te indico en el parcial
-Escribe aquí el que te corresponde.
+from sqlmodel import SQLModel, Field
+import enum
 
-'''
+class EstadoUsuario(str, enum.Enum):
+    activo = "activo"
+    inactivo = "inactivo"
+    suspendido = "suspendido"
+
+class Usuario(SQLModel, table=True):
+    __tablename__ = "usuarios"
+    id: int = Field(default=None, primary_key=True)
+    nombre: str
+    email: str
+    estado: EstadoUsuario 
+    premium: bool = Field(default=False)
