15b7cc10be1d57b7adff997a84b29add67d6e13b
Sebastian lopez <juancholour@gmail.com>
2025-05-06 18:25:35 -0500

    se agregaron los modelos de tareas

--- data/models.py
+++ data/models.py
@@ -1,4 +1,5 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field,Relationship
+from typing import Optional
 import enum
 
 class EstadoUsuario(str, enum.Enum):
@@ -6,10 +7,26 @@ class EstadoUsuario(str, enum.Enum):
     inactivo = "inactivo"
     suspendido = "suspendido"
 
+class EstadoTarea(str, enum.Enum):
+    pendiente = "pendiente"
+    en_ejecucion = "en_ejecucion"
+    realizada = "realizada"
+    cancelada = "cancelada"
+
 class Usuario(SQLModel, table=True):
     __tablename__ = "usuarios"
-    id: int = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     nombre: str
     email: str
     estado: EstadoUsuario 
     premium: bool = Field(default=False)
+    tareas: Optional["Tarea"] = Relationship(back_populates="usuario")
+
+class Tarea(SQLModel, table=True):
+    __tablename__ = "tareas"
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nombre: str
+    descripcion: str
+    estado: EstadoTarea
+    usuario_id: int = Field(foreign_key="usuarios.id",nullable=False)
+    usuario: Optional[Usuario]= Relationship(back_populates="tareas")
