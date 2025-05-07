0b9b5a5a9b90b7b8f990c91aa4fa931c1cb4c4b4
Sebastian lopez <juancholour@gmail.com>
2025-05-06 20:51:26 -0500

    se agrego tangs en los endpoints

--- main.py
+++ main.py
@@ -19,34 +19,34 @@ async def lifespan(app:FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.post("/usuarios/", response_model=Usuario, status_code=status.HTTP_201_CREATED)
+@app.post("/usuarios/", response_model=Usuario, status_code=status.HTTP_201_CREATED, tags=["Usuarios"])
 async def crear_usuario(usuario: UsuarioCreate, session: AsyncSession = Depends(get_session)):
     return await create_user(usuario, session)
 
-@app.get("/usuarios/", response_model=List[Usuario])
+@app.get("/usuarios/", response_model=List[Usuario], tags=["Usuarios"])
 async def obtener_usuarios(session: AsyncSession = Depends(get_session)):
     return await obtener_usuarios_db(session)
 
-@app.get("/usuarios/{usuario_email}", response_model=Usuario)
+@app.get("/usuarios/{usuario_email}", response_model=Usuario, tags=["Usuarios"])
 async def obtener_usuario_por_email(usuario_email:str, session:AsyncSession=Depends(get_session)):
     return await obtener_usuario_por_email_db(usuario_email, session)
 
-@app.put("/usuarios/estado", response_model=Usuario)
+@app.put("/usuarios/estado", response_model=Usuario,tags=["Usuarios"])
 async def actualizar_usuario_db(usuario_email:str, usuario:UsuarioCreate, session:AsyncSession=Depends(get_session)):
     return await actualizar_estado_usuario_db(usuario_email, usuario, session)
 
-@app.patch("/usuarios/{usuario_email}/premium", response_model=Usuario)
+@app.patch("/usuarios/{usuario_email}/premium", response_model=Usuario, tags=["Usuarios"])
 async def actualizar_usuario_premium(usuario_email:str, usuario_premium:bool, session:AsyncSession=Depends(get_session)):
     return await actualizar_usuario_premium_db(usuario_email, usuario_premium, session)
 
-@app.get("/usuarios/Premium/inactivos",response_model=List[Usuario])
+@app.get("/usuarios/Premium/inactivos",response_model=List[Usuario], tags=["Usuarios"])
 async def obtener_usuarios_premium_inactivos_prem(session:AsyncSession=Depends(get_session)):
     usuario_premium_inactivo = await obtener_usuarios_inactivos_premuim_db(session)
     if not usuario_premium_inactivo:
         raise HTTPException(status_code=404, detail="No hay usuarios premium inactivos.")
     return usuario_premium_inactivo
 
-@app.get("/usuarios/inactivos",response_model=List[Usuario])
+@app.get("/usuarios/inactivos",response_model=List[Usuario], tags=["Usuarios"])
 async def obtener_usuarios_inactivos_db(session:AsyncSession=Depends(get_session)):
     usuario_inactivo = await obtener_usuarios_inactivos_db(session)
     if not usuario_inactivo:
